ca39f35b1cd2cda4950abc07e3fd1b3a8fd98eb3
JBT13 <jeremiahborjas@gmail.com>
2026-02-20 13:13:42 +0000

    hello world2

---
+++ Gagnaskipan/assignment2/test.py
@@ -0,0 +1,63 @@
+#
+# Test program to test the SSL, DDL (partially), and Deque implementations.
+# Nice way to do that is to ensure my deque same as Pythons!
+#
+import random
+random.seed(42)  # Fix the seed, such that result is reproducible.
+
+from collections import deque # Python's deque
+
+from dll import DLList # My implementations
+from sll import SLList
+from deque import Deque
+
+def testing_deque(n, lst):
+    d = deque()
+    s = Deque(lst)
+
+    for i in range(n):
+        if random.random() > 0.5:
+            elem = random.randrange(0, 100)
+            if random.random() > 0.5:
+                d.append(elem)
+                s.append(elem)
+            else:
+                d.appendleft(elem)
+                s.appendleft(elem)
+
+        if len(d) == 0:
+            continue
+
+        if random.random() > 0.52:
+            # elem = random.randrange(0, 100)
+            if random.random() > 0.5:
+                d.pop()
+                s.pop()
+            else:
+                d.popleft()
+                s.popleft()
+
+        if len(d) != len(s):
+            print(d, s, len(d), len(s))
+            return False
+
+        if not d != s.is_empty():
+            return False
+
+        if len(d) > 0:
+            if d[0] != s.front():
+                return False
+            if d[-1] != s.back():
+                return False
+
+        if str(list(d)) != str(s):
+            print(str(list(d)), str(s))
+            return False
+
+    return True
+
+if __name__ == '__main__':
+    n = 20000
+    print(testing_deque(n, SLList()))
+    print(testing_deque(n, DLList()))
+
